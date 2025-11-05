0aaf51feb4df34b163c6063f8f112b4498d7622f
Altusha4 <240782@astanait.edu.kz>
2025-11-05 21:56:59 +0600

    main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Shape circle = new Circle();
+        Shape square = new Square();
+
+        circle.draw();
+        square.draw();
+    }
+}
