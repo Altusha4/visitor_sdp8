98b84f5f775b9fb5e1c15838d101c268fbb827fa
Altusha4 <240782@astanait.edu.kz>
2025-11-05 22:06:42 +0600

    creating concrete visitor

--- src/DrawVisitor.java
+++ src/DrawVisitor.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
-public class DrawVisitor {
+public class DrawVisitor implements Visitor {
+    @Override
+    public void visit(Circle circle) {
+        System.out.println("Draw Circle");
+    }
+    @Override
+    public void visit(Square square) {
+        System.out.println("Draw Square");
+    }
 }
